775482bfd2c9e295c197bbdfa4ba5cbcbdf8e336
Jason CA <jasonca2023@gmail.com>
2025-05-24 16:58:23 -0700

    commit

--- Super-Pirate-World/code/enemies.py
+++ Super-Pirate-World/code/enemies.py
@@ -23,9 +23,11 @@ class Tooth(pygame.sprite.Sprite):
 
         floor_rect_right = pygame.FRect(self.rect.bottomright, (1, 1))
         floor_rect_left = pygame.FRect(self.rect.bottomleft, (-1, 1))
+        wall_rect = pygame.FRect(self.rect.topleft + vector(-1, 0), (self.rect.width + 2, 1))
 
         if floor_rect_right.collidelist(self.collision_rects) < 0 and self.direction > 0 or \
-           floor_rect_left.collidelist(self.collision_rects) < 0 and self.direction < 0:
+           floor_rect_left.collidelist(self.collision_rects) < 0 and self.direction < 0 or \
+            wall_rect.collidelist(self.collision_rects) != -1:
             self.direction *= -1
 
 class Shell(pygame.sprite.Sprite):
