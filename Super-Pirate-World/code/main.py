41464ffaca486e93a190c8ccb24b497dd4092f62
Jason CA <jasonca2023@gmail.com>
2025-06-08 19:56:30 -0700

    commit

--- Super-Pirate-World/code/main.py
+++ Super-Pirate-World/code/main.py
@@ -99,7 +99,7 @@ class Game:
 	def check_game_over(self):
 		if self.data.health <= 0:
 			pygame.mixer.fadeout(10000)
-			self.display_surface.fill('#201f1f')
+			self.display_surface.fill('#33323d')
 			lose_text = self.lose_font.render('You lose!', True, '#ebd278')
 			lose_rect = lose_text.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 			self.display_surface.blit(lose_text, lose_rect)
@@ -111,7 +111,7 @@ class Game:
 	def check_win(self):
 		if self.data.current_level == 5 and isinstance((self.current_stage), Overworld):
 			pygame.mixer.fadeout(10000)
-			self.display_surface.fill("#201f1f")
+			self.display_surface.fill('#33323d')
 			win_text = self.win_font.render('YOU WIN!!', True, '#ebd278')
 			win_rect = win_text.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 			self.display_surface.blit(win_text, win_rect)
