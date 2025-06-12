f49f37c3825e61ba01e8a5f45e324c971ef7f6f1
Jason CA <jasonca2023@gmail.com>
2025-06-11 22:05:58 -0700

    commit

--- PirateMaker/code/editor.py
+++ PirateMaker/code/editor.py
@@ -28,6 +28,7 @@ class Editor:
 				sys.exit()
 			self.pan_input(event)
 			self.selection_hotkeys(event)
+			self.menu_click(event)
 	
 	def pan_input(self, event):
 		if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[0]:
@@ -54,6 +55,10 @@ class Editor:
 				self.selection_index -= 1
 		self.selection_index = max(2, min(self.selection_index, len(EDITOR_DATA) - 1))
 
+	def menu_click(self, event):
+		if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint(mouse_pos()):
+			self.selection_index = self.menu.click(mouse_pos(), mouse_buttons())
+
 	def draw_tile_lines(self):
 		cols = WINDOW_WIDTH // TILE_SIZE
 		rows = WINDOW_HEIGHT // TILE_SIZE
@@ -79,3 +84,4 @@ class Editor:
 		self.event_loop()
 		pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
 		self.menu.display()
+		print(self.selection_index)
--- PirateMaker/code/settings.py
+++ PirateMaker/code/settings.py
@@ -4,30 +4,30 @@ WINDOW_HEIGHT = 720
 ANIMATION_SPEED = 8
 
 EDITOR_DATA = {
-	0: {'style': 'player', 'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': '../graphics/player/idle_right'},
+	0: {'style': 'player', 'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': 'pygame/PirateMaker/graphics/player/idle_right'},
 	1: {'style': 'sky', 'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': None},
 	
-	2: {'style': 'terrain', 'type': 'tile', 'menu': 'terrain', 'menu_surf': '../graphics/menu/land.png',  'preview': '../graphics/preview/land.png',  'graphics': None},
-	3: {'style': 'water', 'type': 'tile', 'menu': 'terrain', 'menu_surf': '../graphics/menu/water.png', 'preview': '../graphics/preview/water.png', 'graphics': '../graphics/terrain/water/animation'},
+	2: {'style': 'terrain', 'type': 'tile', 'menu': 'terrain', 'menu_surf': 'pygame/PirateMaker/graphics/menu/land.png',  'preview': 'pygame/PirateMaker/graphics/preview/land.png',  'graphics': None},
+	3: {'style': 'water', 'type': 'tile', 'menu': 'terrain', 'menu_surf': 'pygame/PirateMaker/graphics/menu/water.png', 'preview': 'pygame/PirateMaker/graphics/preview/water.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/water/animation'},
 	
-	4: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': '../graphics/menu/gold.png', 'preview': '../graphics/preview/gold.png', 'graphics': '../graphics/items/gold'},
-	5: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': '../graphics/menu/silver.png', 'preview': '../graphics/preview/silver.png', 'graphics': '../graphics/items/silver'},
-	6: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': '../graphics/menu/diamond.png', 'preview': '../graphics/preview/diamond.png', 'graphics': '../graphics/items/diamond'},
+	4: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'pygame/PirateMaker/graphics/menu/gold.png', 'preview': 'pygame/PirateMaker/graphics/preview/gold.png', 'graphics': 'pygame/PirateMaker/graphics/items/gold'},
+	5: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'pygame/PirateMaker/graphics/menu/silver.png', 'preview': 'pygame/PirateMaker/graphics/preview/silver.png', 'graphics': 'pygame/PirateMaker/graphics/items/silver'},
+	6: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'pygame/PirateMaker/graphics/menu/diamond.png', 'preview': 'pygame/PirateMaker/graphics/preview/diamond.png', 'graphics': 'pygame/PirateMaker/graphics/items/diamond'},
 
-	7:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': '../graphics/menu/spikes.png',      'preview': '../graphics/preview/spikes.png', 'graphics': '../graphics/enemies/spikes'},
-	8:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': '../graphics/menu/tooth.png', 'preview': '../graphics/preview/tooth.png', 'graphics': '../graphics/enemies/tooth/idle'},
-	9:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': '../graphics/menu/shell_left.png',  'preview': '../graphics/preview/shell_left.png', 'graphics': '../graphics/enemies/shell_left/idle'},
-	10: {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': '../graphics/menu/shell_right.png', 'preview': '../graphics/preview/shell_right.png', 'graphics': '../graphics/enemies/shell_right/idle'},
+	7:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'pygame/PirateMaker/graphics/menu/spikes.png', 'preview': 'pygame/PirateMaker/graphics/preview/spikes.png', 'graphics': 'pygame/PirateMaker/graphics/enemies/spikes'},
+	8:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'pygame/PirateMaker/graphics/menu/tooth.png', 'preview': 'pygame/PirateMaker/graphics/preview/tooth.png', 'graphics': 'pygame/PirateMaker/graphics/enemies/tooth/idle'},
+	9:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'pygame/PirateMaker/graphics/menu/shell_left.png', 'preview': 'pygame/PirateMaker/graphics/preview/shell_left.png', 'graphics': 'pygame/PirateMaker/graphics/enemies/shell_left/idle'},
+	10: {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'pygame/PirateMaker/graphics/menu/shell_right.png', 'preview': 'pygame/PirateMaker/graphics/preview/shell_right.png', 'graphics': 'pygame/PirateMaker/graphics/enemies/shell_right/idle'},
 	
-	11: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': '../graphics/menu/small_fg.png', 'preview': '../graphics/preview/small_fg.png', 'graphics': '../graphics/terrain/palm/small_fg'},
-	12: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': '../graphics/menu/large_fg.png', 'preview': '../graphics/preview/large_fg.png', 'graphics': '../graphics/terrain/palm/large_fg'},
-	13: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': '../graphics/menu/left_fg.png', 'preview': '../graphics/preview/left_fg.png', 'graphics': '../graphics/terrain/palm/left_fg'},
-	14: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': '../graphics/menu/right_fg.png', 'preview': '../graphics/preview/right_fg.png', 'graphics': '../graphics/terrain/palm/right_fg'},
+	11: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/small_fg.png', 'preview': 'pygame/PirateMaker/graphics/preview/small_fg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/small_fg'},
+	12: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/large_fg.png', 'preview': 'pygame/PirateMaker/graphics/preview/large_fg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/large_fg'},
+	13: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/left_fg.png', 'preview': 'pygame/PirateMaker/graphics/preview/left_fg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/left_fg'},
+	14: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/right_fg.png', 'preview': 'pygame/PirateMaker/graphics/preview/right_fg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/right_fg'},
 
-	15: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': '../graphics/menu/small_bg.png', 'preview': '../graphics/preview/small_bg.png', 'graphics': '../graphics/terrain/palm/small_bg'},
-	16: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': '../graphics/menu/large_bg.png', 'preview': '../graphics/preview/large_bg.png', 'graphics': '../graphics/terrain/palm/large_bg'},
-	17: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': '../graphics/menu/left_bg.png',  'preview': '../graphics/preview/left_bg.png', 'graphics': '../graphics/terrain/palm/left_bg'},
-	18: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': '../graphics/menu/right_bg.png', 'preview': '../graphics/preview/right_bg.png', 'graphics': '../graphics/terrain/palm/right_bg'}
+	15: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/small_bg.png', 'preview': 'pygame/PirateMaker/graphics/preview/small_bg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/small_bg'},
+	16: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/large_bg.png', 'preview': 'pygame/PirateMaker/graphics/preview/large_bg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/large_bg'},
+	17: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/left_bg.png', 'preview': 'pygame/PirateMaker/graphics/preview/left_bg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/left_bg'},
+	18: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'pygame/PirateMaker/graphics/menu/right_bg.png', 'preview': 'pygame/PirateMaker/graphics/preview/right_bg.png', 'graphics': 'pygame/PirateMaker/graphics/terrain/palm/right_bg'}
 }
 
 NEIGHBOR_DIRECTIONS = {
@@ -54,3 +54,5 @@ SEA_COLOR = '#92a9ce'
 HORIZON_COLOR = '#f5f1de'
 HORIZON_TOP_COLOR = '#d1aa9d'
 LINE_COLOR = 'black'
+BUTTON_BG_COLOR = '#33323d'
+BUTTON_LINE_COLOR = '#f5f1de'
