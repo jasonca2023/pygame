80f1cc86d77a035398a8bccaba61a7ae8461fec7
Jason CA <jasonca2023@gmail.com>
2025-06-24 15:05:11 -0700

    commit

--- Super-Pirate-World/code/support.py
+++ Super-Pirate-World/code/support.py
@@ -18,9 +18,10 @@ def import_folder_dict(*path):
 	frame_dict = {}
 	for folder_path, _, image_names in walk(join(*path)):
 		for image_name in image_names:
-			full_path = join(folder_path, image_name)
-			surface = pygame.image.load(full_path).convert_alpha()
-			frame_dict[image_name.split('.')[0]] = surface
+			if image_name.endswith('.png'):
+				full_path = join(folder_path, image_name)
+				surface = pygame.image.load(full_path).convert_alpha()
+				frame_dict[image_name.split('.')[0]] = surface
 	return frame_dict
 
 def import_sub_folders(*path):
