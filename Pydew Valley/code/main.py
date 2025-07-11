d03343349c3ed8433900481b2094c537458796aa
Jason CA <jasonca2023@gmail.com>
2025-07-11 15:31:40 -0700

    commit

--- Pydew Valley/code/main.py
+++ Pydew Valley/code/main.py
@@ -5,7 +5,7 @@ from level import Level
 class Game:
 	def __init__(self):
 		pygame.init()
-		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 		pygame.display.set_caption('Sprout land')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
