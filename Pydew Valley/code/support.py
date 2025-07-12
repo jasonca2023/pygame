fe60883faa5fa0b776ad5cda5ff33f8cf4cbc667
Jason CA <jasonca2023@gmail.com>
2025-07-12 16:19:17 -0700

    commit

--- Pydew Valley/code/support.py
+++ Pydew Valley/code/support.py
@@ -4,7 +4,7 @@ from os import walk
 def import_folder(path):
     surface_list = []
     for _, __, img_files in walk(path):
-        img_files.sort(key = lambda x: int(x.split('.')[0]))
+        img_files.sort(key = lambda name: int(name.split('.')[0]))
         for image in img_files:
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
