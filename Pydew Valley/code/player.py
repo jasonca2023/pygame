91e64fdd2558d1ffad018a7f2beef5e7642f0b45
Jason CA <jasonca2023@gmail.com>
2025-07-11 20:22:36 -0700

    commit

--- Pydew Valley/code/player.py
+++ Pydew Valley/code/player.py
@@ -1,17 +1,34 @@
 import pygame
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('red')
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
 
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+    def import_assets(self):
+        self.animations = {
+            'up': [], 'down': [], 'left': [], 'right': [],
+            'up_idle': [], 'down_idle': [], 'left_idle': [], 'right_idle': [],
+            'up_hoe': [], 'down_hoe': [], 'left_hoe': [], 'right_hoe': [],
+            'up_axe': [], 'down_axe': [], 'left_axe': [], 'right_axe': [],
+            'up_water': [], 'down_water': [], 'left_water': [], 'right_water': []
+        }
+
+        for animation in sorted(self.animations.keys()):
+            full_path = '../graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -36,7 +53,7 @@ class Player(pygame.sprite.Sprite):
 
         self.pos.x += self.direction.x * self.speed * dt
         self.rect.centerx = self.pos.x
-        
+
         self.pos.y += self.direction.y * self.speed * dt
         self.rect.centery = self.pos.y
 
