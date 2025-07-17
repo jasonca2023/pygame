faf8b14748bf44f9fc22657d03bbf9975135529e
Jason CA <jasonca2023@gmail.com>
2025-07-16 20:50:08 -0700

    commit

--- Pydew Valley/code/level.py
+++ Pydew Valley/code/level.py
@@ -7,6 +7,8 @@ from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
 from soil import SoilLayer
+from sky import Rain
+from random import randint
 
 class Level:
 	def __init__(self):
@@ -21,6 +23,10 @@ class Level:
 		self.setup()
 		self.overlay = Overlay(self.player)
 		self.transition = Transition(self.reset, self.player)
+
+		self.rain = Rain(self.all_sprites)
+		self.raining = randint(0, 10) > 3
+		self.soil_layer.raining = self.raining
 
 	def setup(self):
 		tmx_data = load_pygame('../data/map.tmx')
@@ -85,6 +91,10 @@ class Level:
 
 	def reset(self):
 		self.soil_layer.remove_water()
+		self.raining = randint(0, 10) > 3
+		self.soil_layer.raining = self.raining
+		if self.raining:
+			self.soil_layer.water_all()
 
 		for tree in self.tree_sprites.sprites():
 			for apple in tree.apple_sprites.sprites():
@@ -99,6 +109,9 @@ class Level:
 		self.all_sprites.update(dt)
 
 		self.overlay.display()
+
+		if self.raining:
+			self.rain.update()
 
 		if self.player.sleep:
 			self.transition.play()
