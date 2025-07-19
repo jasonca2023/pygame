fba0032e29d91781b98350cb6e0156f9e23ac35c
Jason CA <jasonca2023@gmail.com>
2025-07-19 15:54:15 -0700

    commit

--- Pydew Valley/code/sky.py
+++ Pydew Valley/code/sky.py
@@ -4,6 +4,21 @@ from support import import_folder
 from sprites import Generic
 from random import randint, choice
 
+class Sky:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        self.full_surf = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.start_color = [255, 255, 255]
+        self.end_color = [60, 80, 90]
+
+    def display(self, dt):
+        for index, value in enumerate(self.end_color):
+            if self.start_color[index] > value:
+                self.start_color[index] -= 0.35 * dt
+
+        self.full_surf.fill(self.start_color)
+        self.display_surface.blit(self.full_surf, (0, 0), special_flags = pygame.BLEND_RGBA_MULT)
+
 class Drop(Generic):
     def __init__(self, surf, pos, moving, groups, z):
         super().__init__(pos, surf, groups, z)
